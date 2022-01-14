45c85fbf50404fdb560506bbb2a89a1db6a9bc9b
eduardorodmol <eduardo.rodriguez.molina@hotmail.es>
2022-01-14 15:48:16 +0100

    HerokuApi

--- API/database/mongo.py
+++ API/database/mongo.py
@@ -8,8 +8,8 @@ password = os.getenv("MONGO_PASS")
 #esta url posteriormente habria que facilitarla como parametro
 
 url = f'mongodb+srv://{username}:{password}@cluster0.l7ahf.mongodb.net'
-
-client = MongoClient(url)
+MONGO_DB_URL = os.getenv("MONGODB_URL") or url
+client = MongoClient(host = MONGO_DB_URL)
 db = client.get_database("midproject")
 
 
